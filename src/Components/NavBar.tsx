effb1ea9e01600a8099645b0d6b8ac18000bf484
fouadGeek <elayachifouad@gmail.com>
2020-10-01 22:00:38 +0100

    Create NavBar Component and Improve styel

---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import { Grid, Button, AppBar, Toolbar, Avatar} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        row:{
+            flexGrow:1
+        },
+        grow:{
+            flexGrow:1
+        },
+        container:{
+            width:1170,
+            margin:"auto"
+        },
+        buttonFontSize:{
+            fontSize:"11px",
+            color:"#a1a1a1"
+        },
+
+        AppBar:{
+            backgroundColor:theme.palette.background.paper,
+            backgroundSize:"cover"
+        },
+        mainLogo:{
+            color: "#a1a1a1",
+            justifyContent:"left",
+            '&:hover':{
+                background:"transparent"
+            }
+        },
+
+        avatar:{
+            height:"100%",
+            borderRadius:0,
+
+
+        },
+
+        loginButton:{
+            background:"#36d613",
+            color:"#fff",
+            borderRadius:"25px",
+            padding:"0px 25px",
+
+            '&:hover':{
+                background: 'green',
+                boxShadow: "0px 2px 10px #888888"
+            }
+        }
+    }),
+);
+
+const NavBar: React.FC = () => {
+    const classes = useStyles();
+    return (
+        <AppBar position="sticky" color="default" className={classes.AppBar}>
+            <Grid item sm={12} xs={12} className={classes.container}>
+                <Toolbar>
+                    <Grid className={classes.grow}>
+                        <Button className={classes.mainLogo}>
+                            <Avatar src="https://upload.wikimedia.org/wikipedia/fr/c/c8/Rick_and_Morty_logo.png" className={classes.avatar} />
+                        </Button>
+                    </Grid>
+                    <Button color="inherit" className={classes.buttonFontSize}>Episodes</Button>
+                    <Button color="inherit" className={classes.buttonFontSize}>Favourites</Button>
+                    <Button color="inherit" className={classes.loginButton}>Login</Button>
+                </Toolbar>
+            </Grid>
+        </AppBar>
+    );
+}
+
+export default NavBar;
